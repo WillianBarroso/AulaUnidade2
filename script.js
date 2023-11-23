711e658efee30003f20710392e494cbdccb1cd9a
Willian Barroso <w.barroso@unesp.br>
2023-11-23 10:08:45 -0300

    Limpar pesquisa

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ btn.addEventListener("click", function () {
     }  
     tarefa.value = "";
     
+    const close = document.querySelectorAll(".close");
+    for(let i=0; i<close.length; i++){
+        close[i].addEventListener("click", function(){
+            close[i].parentElement.remove();
+        });
+    }
+
     lista.addEventListener("click", function(e){
         e.target.parentElement.querySelector(".check").style.color = "#349223";
         e.target.parentElement.querySelector("span").style.textDecoration = "line-through";
