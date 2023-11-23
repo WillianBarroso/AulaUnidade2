1e67d6107faf37d5a3d9da0d0ca9549e39b8c8d3
Heloisa Felizardo <helocmp@yahoo.com.br>
2023-11-23 13:44:59 -0300

    fix bugs

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-const tarefa = docment.querySelector("#tarefa");
-const btn = document.querySelector("#btn");
-const lista = document.querySelector("#lista");
-const close = document.querySelectorAll(".close");
+const tarefa = document.querySelector('#tarefa');
+const btn = document.querySelector('#btn');
+const lista = document.querySelector('#lista');
+const close = document.querySelectorAll('.close');
 
-btn.addEventListener("click", function () {
-    if (tarefa.value == "") {
-        alert("Digite alguma tarefa");
-    } else {
-        lista.innerHTML += '<li><i class="fas fa-check-circle check"> </i><span>${ tarefa.value }</span><i class="fa-solid fa-trash-can close"></i></li>';
-            }  
-    tarefa.value = "";
-    
-    const close = document.querySelectorAll(".close");
-    for(let i=0; i<close.length; i++){
-        close[i].addEventListener("click", function(){
-            close[i].parentElement.remove();
-        });
-    }
+btn.addEventListener('click', function () {
+	if (tarefa.value == '') {
+		alert('Digite alguma tarefa');
+	} else {
+		lista.innerHTML += `<li><i class="fas fa-check-circle check"> </i><span>${tarefa.value}</span><i class="fa-solid fa-trash-can close"></i></li>`;
+	}
+	tarefa.value = '';
 
-    lista.addEventListener("click", function(e){
-        e.target.parentElement.querySelector(".check").style.color = "#349223";
-        e.target.parentElement.querySelector("span").style.textDecoration = "line-through";
-    });
+	const close = document.querySelectorAll('.close');
+	for (let i = 0; i < close.length; i++) {
+		close[i].addEventListener('click', function () {
+			close[i].parentElement.remove();
+		});
+	}
+
+	lista.addEventListener('click', function (e) {
+		e.target.parentElement.querySelector('.check').style.color = '#349223';
+		e.target.parentElement.querySelector('span').style.textDecoration = 'line-through';
+	});
 });
 
 close.forEach((item) => {
-    console.log (item);
-    item.addEventListener("click", function (e)) {
-        console.log(e);
-        item.parentElement.remove();
-        console.log(close);
-    }
+	console.log(item);
+	item.addEventListener('click', (e) => {
+		console.log(e);
+		item.parentElement.remove();
+		console.log(close);
+	});
 });
