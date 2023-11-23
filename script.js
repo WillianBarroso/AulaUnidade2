f935890f4aed3db35483ad3ebcb3b18142cde469
Willian Barroso <w.barroso@unesp.br>
2023-11-23 13:23:26 -0300

    Correção

--- script.js
+++ script.js
@@ -7,12 +7,8 @@ btn.addEventListener("click", function () {
     if (tarefa.value == "") {
         alert("Digite alguma tarefa");
     } else {
-        lista.innerHTML += ´<li>
-                <i class="fas fa-check-circle check"></i>
-                <span>${ tarefa.value }</span>
-                <i class="fa-solid fa-trash-can close"></i>
-            </li>´;
-    }  
+        lista.innerHTML += '<li><i class="fas fa-check-circle check"> </i><span>${ tarefa.value }</span><i class="fa-solid fa-trash-can close"></i></li>';
+            }  
     tarefa.value = "";
     
     const close = document.querySelectorAll(".close");
