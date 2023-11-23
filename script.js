6d235da1b2fe6f3f0be4cc98f4496454be208fc0
Willian Barroso <w.barroso@unesp.br>
2023-11-23 09:47:07 -0300

    Unidades 2 e 3 - Programação WEB

---
+++ script.js
@@ -0,0 +1,31 @@
+const tarefa = docment.querySelector("#tarefa");
+const btn = document.querySelector("#btn");
+const lista = document.querySelector("#lista");
+const close = document.querySelectorAll(".close");
+
+btn.addEventListener("click", function () {
+    if (tarefa.value == "") {
+        alert("Digite alguma tarefa");
+    } else {
+        lista.innerHTML += ´<li>
+                <i class="fas fa-check-circle check"></i>
+                <span>${ tarefa.value }</span>
+                <i class="fa-solid fa-trash-can close"></i>
+            </li>´;
+    }  
+    tarefa.value = "";
+    
+    lista.addEventListener("click", function(e){
+        e.target.parentElement.querySelector(".check").style.color = "#349223";
+        e.target.parentElement.querySelector("span").style.textDecoration = "line-through";
+    });
+});
+
+close.forEach((item) => {
+    console.log (item);
+    item.addEventListener("click", function (e)) {
+        console.log(e);
+        item.parentElement.remove();
+        console.log(close);
+    }
+});
